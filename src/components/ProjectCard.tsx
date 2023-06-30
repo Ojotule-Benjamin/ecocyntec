153bf0aa10d26c89667b439d9b3f7b6cb59a8dee
Ojotule Benjamin Ajodo <benjamin.ojotule.ajodo@gmail.com>
2023-06-30 16:31:01 +0100

    savaged the situation

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import CustomButton from "./CustomButton";
 
 type ProjectCardProps = {
-  src?: string;
+  src: string;
   alt?: string;
-  title?: string;
+  title: string;
   location?: string;
+  className?: string;
   onClick?: () => void;
 };
 
@@ -14,15 +15,18 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   alt,
   title,
   location,
+  className,
   onClick,
 }) => {
   return (
-    <div className=" w-full  md:w-96 md:h-96 p-5 md:p-0 bg-backgroundColor">
+    <div className={`w-full  md:w-96 md:h-96 p-5 md:p-0 bg-backgroundColor `}>
       <div className=" h-96 md:h-[500px] relative rounded-md border-4 ">
         <div className="w-full h-full">
           <img src={src} alt={alt} className=" w-full h-full object-cover" />
         </div>
-        <div className="w-4/5 h-24 md:w-4/5  md:h-40 pl-3 md:pl-7 pt-2 md:pt-5 flex flex-col bg-white absolute bottom-0">
+        <div
+          className={`w-4/5 h-24 hover:h-28 md:w-4/5 md:h-28 hover:md:h-36 pl-3 hover:pl-5 md:pl-4 hover:md:pl-7 duration-500 ease-linear gap-0 hover:md:gap-2 p-2 md:p-0 md:pt-3 flex flex-col bg-white border-2 border-primaryColor hover:rounded-tr-3xl absolute bottom-0 ${className}`}
+        >
           <h1 className=" font-CrimsonText font-bold text-base md:text-2xl text-primaryColor">
             {title}
           </h1>
@@ -31,7 +35,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
           </p>
           <CustomButton
             title="VIEW PROJECT"
-            className="w-28 h-28 md:w-[154px] md:h-[51px] text-xs md:text-base text-white cursor-pointer border-2  border-white"
+            className="w-24 h-9 md:w-[120px] md:h-[40px] text-[10px] md:text-xs text-white cursor-pointer border-2  border-white"
             titleStyle=" text-400"
             onClick={onClick}
           />
